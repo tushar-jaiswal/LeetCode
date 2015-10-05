42e22e2aa58e5c9846ccdf93f23ca67806d890e7
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2015-10-04 23:04:19 -0500

    Optimal solution for Longest substring problem
    O(n) time complexity and O(n) space complexity solution for the "longest substring without repeating characters" problem.

--- Algorithms/3. Longest substring without repeating characters/Longest Substring.java
+++ Algorithms/3. Longest substring without repeating characters/Longest Substring.java
@@ -2,65 +2,21 @@
 //Creation Date: 10/04/2015
 
 /*Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
-This input hits all conditions: "abdvaabfeghij". The longest substring is ""abfeghij"", with the length of 8
+This input hits all conditions for the optimal solution: "abdvaabfeghij". The longest substring is ""abfeghij"", with the length of 8
 */
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        HashMap<Character, Integer> substringChars = new HashMap<Character, Integer>();
-        int longestLength = 0;
+        Map<Character,Integer> substringChars = new HashMap<Character,Integer>();
+        int start = 0;
         int length = 0;
-        //more concise for(char c: s.toCharArray()) - more concise but cost penalty to create a newly allocated character array whose length is the length of this string and whose contents are initialized to contain the character sequence represented by this string
-        for(int i = 0; i < s.length(); i++ )
-        {
-            if(longestLength < (s.substring(i)).length() + length)
-            {
-                if(substringChars.get(s.charAt(i)) != null)
-                {
-                    substringChars.clear();
-                    length = 1;
-                    substringChars.put(s.charAt(i), i);
-                }
-                else
-                {
-                    substringChars.put(s.charAt(i), i);
-                    length++;
-                    if(length > longestLength)
-                    {
-                        longestLength = length;   
-                    }
-                    
-                    String substring = s.substring(i+1);
-                    if(longestLength < substring.length())
-                    {
-                        HashMap<Character, Integer> substringChars2 = new HashMap<Character, Integer>();
-                        int length2 = 0;
-                        for (int j = i+1; j < substring.length(); j++)
-                        {
-                            if(substringChars2.get(s.charAt(i)) != null)
-                            {
-                                break;
-                            }
-                            else
-                            {
-                                substringChars2.put(s.charAt(i), i);
-                                length2++;
-                                if(length2 > longestLength)
-                                {
-                                    longestLength = length2;   
-                               }
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                substringChars.clear();
-                length = 1;
-                substringChars.put(s.charAt(i), i);
-            }
+        for(int i = 0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(substringChars.containsKey(c) && start<substringChars.get(c)+1) 
+            { start = substringChars.get(c)+1; }
+            length = Math.max(length, i-start+1);
+            substringChars.put(c,i);
         }
-        return longestLength;
+        return length;
     }
 }
