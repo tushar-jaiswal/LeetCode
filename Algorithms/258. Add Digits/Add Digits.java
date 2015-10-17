506f637c9e7fad68285cc6b66057cb8f3dd6ce7c
Tushar Jaiswal <tjaiswal3@gatech.edu>
2015-10-17 17:33:21 -0500

    Optimal solution for the "Add Digits" problem

--- Algorithms/258. Add Digits/Add Digits.java
+++ Algorithms/258. Add Digits/Add Digits.java
@@ -2,25 +2,18 @@
 //Creation Date: 10/17/2015
 
 /*Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
-
-For example:
-
-Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.*/
+For example: Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+Follow up: Could you do it without any loop/recursion in O(1) runtime?*/
 public class Solution {
     public int addDigits(int num) {
-        int copy;
-        
-        while(num / 10 != 0)
-        {
-            copy = num;
-            num = 0;
-            do
-            {
-                num += copy % 10;
-                copy /= 10;
-            }while(copy / 10 != 0);
-            num += copy;
+        if(num % 9 == 0)
+        { 
+            if(num == 0)
+            { return 0;}
+            else
+            { return 9;}
         }
-        return num;
+        else
+        { return num % 9; }
     }
 }
