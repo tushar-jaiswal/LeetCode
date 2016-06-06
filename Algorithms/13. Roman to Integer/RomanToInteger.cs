eee194e53156304a562580972a2d60382d4cb6dc
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-05 21:43:40 -0500

    Created RomanToInteger.cs
    c# solution for Roman to Integer problem.
    Problem Source: https://leetcode.com/problems/roman-to-integer/

---
+++ Algorithms/13. Roman to Integer/RomanToInteger.cs
@@ -0,0 +1,42 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/05/2016
+
+/*Given a roman numeral, convert it to an integer.
+
+Input is guaranteed to be within the range from 1 to 3999.*/
+public class Solution {
+    public int RomanToInt(string s) {
+        int i, result=0, first, second;
+        s = s.ToUpper();
+        
+        for(i = 0; i < s.Length; i++)
+        {
+            first = romanCharToInt(s[i]);
+            if(i+1 < s.Length)
+            {
+                second = romanCharToInt(s[i+1]);
+                if(first < second)
+                {
+                    result += second - first;
+                    i++;
+                    continue;
+                }
+            }
+            
+            result += first;
+        }
+        return result;
+    }
+    
+    private int romanCharToInt(char c)
+    {
+        if(c == 'I') { return 1; }
+        if(c == 'V') { return 5; }
+        if(c == 'X') { return 10; }
+        if(c == 'L') { return 50; }
+        if(c == 'C') { return 100; }
+        if(c == 'D') { return 500; }
+        if(c == 'M') { return 1000; }
+        else return 0;
+    }
+}
