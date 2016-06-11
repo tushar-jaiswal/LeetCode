36a0a6c024b1db881a3330f7e7ae34a585c46d6e
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-11 15:11:50 -0500

    Created ValidParentheses.cs
    c# solution for the Valid Parentheses problem.
    Problem Source: https://leetcode.com/problems/valid-parentheses/

---
+++ Algorithms/20. Valid Parentheses/ValidParentheses.cs
@@ -0,0 +1,49 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/11/2016
+
+/*Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.*/
+public class Solution {
+    public bool IsValid(string s) {
+        Stack stack = new Stack();
+        foreach(char c in s)
+        {
+            char peek = '0';
+            if(stack.Count != 0)
+            { peek = (char)stack.Peek(); }
+            if(stack.Count != 0 && peek == c)
+            {
+                stack.Pop();
+            }
+            else
+            {
+                stack.Push(getCounterBracket(c));
+            }
+        }
+        if(stack.Count == 0)
+        { return true; }
+        else 
+        { return false; }
+    }
+    
+    private char getCounterBracket(char c)
+    {
+        switch (c)
+        {
+            case '{': return '}';
+                    break;
+            case '}': return '{';
+                    break;
+            case '[': return ']';
+                    break;
+            case ']': return '[';
+                    break;
+            case '(': return ')';
+                    break;
+            case ')': return '(';
+                    break;
+            default: return '0';
+        }
+    }
+}
