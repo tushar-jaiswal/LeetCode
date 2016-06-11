ee4b65ea3e108438087f867d60612be235c302f9
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-11 13:09:31 -0500

    Created 19. RemoveNthNode-SinglePass.cs
    c# single pass solution for the Remove nth node from end of list
    problem.
    Problem Source:
    https://leetcode.com/problems/remove-nth-node-from-end-of-list/

---
+++ Algorithms/19. Remove Nth Node From End of List/19. RemoveNthNode-SinglePass.cs
@@ -0,0 +1,46 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/11/2016
+
+/*Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+   Given linked list: 1->2->3->4->5, and n = 2.
+
+   After removing the second node from the end, the linked list becomes 1->2->3->5.
+Note:
+Given n will always be valid.
+Try to do this in one pass.*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        ListNode dummy, first, second;
+        dummy = new ListNode(0);
+        dummy.next = head;
+        first = dummy;
+        second = dummy;
+        
+        int i;
+        for(i = 0; i <= n; i++)
+        {
+            second = second.next;
+        }
+        
+        while(second != null)
+        {
+            first = first.next;
+            second = second.next;
+        }
+        
+        first.next = first.next.next;
+        return dummy.next;
+    }
+}
