279dc0504e5db5b3b001dbb4d74e82478ba84f69
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-12 17:32:25 -0500

    Created RemoveDuplicatesSortedArray.java
    Java solution for remove duplicates from sorted array problem.
    Problem Source:
    https://leetcode.com/problems/remove-duplicates-from-sorted-array/

---
+++ Algorithms/26. Remove Duplicates from Sorted Array/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,8 @@
+possible movies from vudu
+Troy/Alexander double feature - $10 - http://www.vudu.com/movies/#!content/500126/Troy-Directors-Cut-Alexander-Revisited-The-Final-Cut-Unrated-Double-Feature
+
+Matrix Series
+Jurassic Park
+Titanic
+Rocky
+Harry Potter
