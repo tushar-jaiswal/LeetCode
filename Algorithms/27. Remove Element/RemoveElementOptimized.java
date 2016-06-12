98e011e62dd906fd198e6dd314442d6ae9eadbb1
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-12 18:54:53 -0500

    Created RemoveElementOptimized.java
    Optimized java solution for the remove element problem.
    Problem Source: https://leetcode.com/problems/remove-element/

---
+++ Algorithms/27. Remove Element/RemoveElementOptimized.java
@@ -0,0 +1,31 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/12/2016
+/*Given an array and a value, remove all instances of that value in place and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example:
+Given input array nums = [3,2,2,3], val = 3
+
+Your function should return length = 2, with the first two elements of nums being 2.
+Hint:
+
+Try two pointers.
+Did you use the property of "the order of elements can be changed"?
+What happens when the elements to remove are rare?*/
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int removed=0;
+        for(int i = 0; i < nums.length-removed; i++)
+        {
+            while(nums[i] == val && nums.length-1-removed >= i)
+            {
+                nums[i] = nums[nums.length-1-removed];
+                removed++;
+            }
+        }
+        return nums.length-removed;
+    }
+}
