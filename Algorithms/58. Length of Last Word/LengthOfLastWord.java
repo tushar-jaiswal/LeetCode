0e244909b6325fecbd9fd7f493e8025cb3498652
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-26 19:27:20 -0500

    Created LengthOfLastWord.java
    Java solution for the Length of Last Word problem.
    Problem Source: https://leetcode.com/problems/length-of-last-word/

---
+++ Algorithms/58. Length of Last Word/LengthOfLastWord.java
@@ -0,0 +1,35 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/26/2016
+/*Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+
+If the last word does not exist, return 0.
+
+Note: A word is defined as a character sequence consists of non-space characters only.
+
+For example, 
+Given s = "Hello World",
+return 5.*/
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0, prevLength = 0;
+        for(int i = 0; i < s.length(); i++)
+        {
+           if(s.charAt(i) != ' ')
+           {
+               length++;
+           }
+           else
+           {
+               if(length != 0)
+               {
+                   prevLength = length;
+               }
+               length = 0;
+           }
+        }
+        if(length == 0 && prevLength != 0)
+        { return prevLength; }
+        else
+        { return length; }
+    }
+}
