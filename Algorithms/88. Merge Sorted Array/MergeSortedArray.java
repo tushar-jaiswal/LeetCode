78844b3b9a74e51e197a1f96be1010f4d9a1b5af
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-28 23:35:48 -0500

    Created MergeSortedArray.java
    Java solution for the Merge Sorted Array problem.
    Problem Source: https://leetcode.com/problems/merge-sorted-array/

---
+++ Algorithms/88. Merge Sorted Array/MergeSortedArray.java
@@ -0,0 +1,34 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/28/2016
+/*Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+Note: You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.*/
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0, j=0, k=0;
+        int[] nums3 = new int[m+n];
+        while(i < m && j < n)
+        {
+            if(nums1[i] < nums2[j])
+            {
+                nums3[k++] = nums1[i++];
+            }
+            else
+            {
+                nums3[k++] = nums2[j++];
+            }
+        }
+        while(i < m)
+        {
+            nums3[k++] = nums1[i++];
+        }
+        while(j < n)
+        {
+            nums3[k++] = nums2[j++];
+        }
+        
+        for(k = 0; k < m+n; k++)
+        {
+            nums1[k] = nums3[k];
+        }
+    }
+}
