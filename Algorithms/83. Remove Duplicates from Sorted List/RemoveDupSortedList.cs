8510686a5a1e4ac179ce1ec6b711bfe8c7e18f20
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-28 23:13:00 -0500

    cleaned the code
    Cleaned the code by removing extra conditions and making the program
    simpler to read.

--- Algorithms/83. Remove Duplicates from Sorted List/RemoveDupSortedList.cs
+++ Algorithms/83. Remove Duplicates from Sorted List/RemoveDupSortedList.cs
@@ -17,19 +17,11 @@ public class Solution {
         ListNode curr = head;
         while(curr!= null && curr.next != null)
         {
-            bool isDuplicate = false;
-            do{
-                if(curr.next != null && curr.val == curr.next.val)
-                {
-                    curr.next = curr.next.next;
-                    isDuplicate = true;
-                }
-                else
-                {
-                    isDuplicate = false;
-                }
-            }while(isDuplicate);
-            if(curr.next != null)
+            if(curr.val == curr.next.val)
+            {
+                curr.next = curr.next.next;
+            }
+            else
             { 
                 curr = curr.next;
             }
