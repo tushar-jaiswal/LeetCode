34b710b6d9a55c438d4dc180a1933c8245126f39
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-06-28 23:33:44 -0500

    Fixed bad commit

--- Algorithms/26. Remove Duplicates from Sorted Array/RemoveDuplicatesSortedArray.java
+++ Algorithms/26. Remove Duplicates from Sorted Array/RemoveDuplicatesSortedArray.java
@@ -1,8 +1,32 @@
-possible movies from vudu
-Troy/Alexander double feature - $10 - http://www.vudu.com/movies/#!content/500126/Troy-Directors-Cut-Alexander-Revisited-The-Final-Cut-Unrated-Double-Feature
+//Author: Tushar Jaiswal
+//Creation Date: 06/12/2016
+/*Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 
-Matrix Series
-Jurassic Park
-Titanic
-Rocky
-Harry Potter
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.*/
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int count = 0;
+        if(nums.length == 0)
+        { return count; }
+        else
+        { count = 1;}
+        for(int i=0, j=1; i < nums.length-1 && j < nums.length; i++)
+        {
+            while(j < nums.length && nums[j] == nums[i])
+            {
+                j++;
+            }
+            if(j == nums.length)
+            { break; }
+            nums[i+1] = nums[j++];
+            count++;
+        }
+        
+        return count;
+    }
+}
