4c9efa94b82a394228eea977f5cfbcad353c2756
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-07-13 00:14:56 -0500

    Created MaximumSubarray.cs
    c# solution for the Maximum Subarray problem.
    Problem Source: https://leetcode.com/problems/maximum-subarray/

---
+++ Algorithms/53. Maximum Subarray/MaximumSubarray.cs
@@ -0,0 +1,59 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/13/2016
+/*Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.*/
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int p1=0, p2=0, sum, maxP1=0, maxP2=0, maxSum;
+        
+        if(nums.Length == 0)
+        {
+            return 0;
+        }
+        
+        bool restartSum = true;
+        sum = 0;
+        maxSum = nums[0];
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(restartSum)
+            {
+                sum = 0;
+            }
+            sum += nums[i];
+            
+            if((sum < sum - nums[i]) && (sum < 0))
+            {
+                if((i + 1) < nums.Length)
+                {
+                    p1 = i + 1;
+                    p2 = i + 1;
+                    restartSum = true;
+                }
+            }
+            else
+            {
+                p2 = i;
+                restartSum = false;
+            }
+            
+            if(sum > maxSum)
+            {
+                maxSum = sum;
+                maxP1 = p1;
+                maxP2 = p2;
+            }
+            if(nums[i] > maxSum)
+            {
+                maxSum = nums[i];
+                maxP1 = i;
+                maxP2 = i;
+                p1 = i;
+                p2 = i;
+            }
+        }
+        
+        return maxSum;
+    }
+}
