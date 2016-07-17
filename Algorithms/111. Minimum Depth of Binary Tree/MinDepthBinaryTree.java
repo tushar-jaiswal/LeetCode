714105f2e7eff5352948298b4aecbef9f3e2593c
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-07-17 08:33:27 -0500

    Making code simpler
    Cleaning up code to make it simpler

--- Algorithms/111. Minimum Depth of Binary Tree/MinDepthBinaryTree.java
+++ Algorithms/111. Minimum Depth of Binary Tree/MinDepthBinaryTree.java
@@ -13,19 +13,18 @@ The minimum depth is the number of nodes along the shortest path from the root n
  */
 public class Solution {
     public int minDepth(TreeNode root) {
-        int noNodesInLevel, noNodesInNextLevel, depth = 0;
+        int depth = 0;
         Queue<TreeNode> nodes = new LinkedList<TreeNode>();
         
-        if(root == null)
-        { return 0; }
-        
-        nodes.add(root);
-        noNodesInLevel = 1;
+        if(root != null)
+        {
+            nodes.add(root);
+        }
         
         while(!nodes.isEmpty())
         {
             depth++;
-            noNodesInNextLevel = 0;
+            int noNodesInLevel = nodes.size();
             for(int i = 0; i < noNodesInLevel; i++)
             {
                 TreeNode node = nodes.remove();
@@ -36,15 +35,12 @@ public class Solution {
                 if(node.left != null)
                 { 
                     nodes.add(node.left);
-                    noNodesInNextLevel++; 
                 }
                 if(node.right != null)
                 {
                     nodes.add(node.right);
-                    noNodesInNextLevel++;
                 }
             }
-            noNodesInLevel = noNodesInNextLevel;
         }
         
         return depth;
