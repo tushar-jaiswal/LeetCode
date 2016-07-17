350eb3d1705ddfca5586c412eb0b5e81cfae1c3d
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-07-17 08:23:46 -0500

    Created MinDepthBinaryTree.cs
    c# solution for the Minimum Depth of Binary Tree problem.
    Problem Source:
    https://leetcode.com/problems/minimum-depth-of-binary-tree/

---
+++ Algorithms/111. Minimum Depth of Binary Tree/MinDepthBinaryTree.cs
@@ -0,0 +1,52 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/17/2016
+/*Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int MinDepth(TreeNode root) {
+        int noNodesInLevel, noNodesInNextLevel, depth = 0;
+        Queue<TreeNode> nodes = new Queue<TreeNode>();
+        
+        if(root == null)
+        { return 0; }
+        
+        nodes.Enqueue(root);
+        noNodesInLevel = 1;
+        
+        while(nodes.Count != 0)
+        {
+            depth++;
+            noNodesInNextLevel = 0;
+            for(int i = 0; i < noNodesInLevel; i++)
+            {
+                TreeNode node = nodes.Dequeue();
+                if(node.left == null && node.right == null)
+                {
+                    return depth;
+                }
+                if(node.left != null)
+                { 
+                    nodes.Enqueue(node.left);
+                    noNodesInNextLevel++; 
+                }
+                if(node.right != null)
+                {
+                    nodes.Enqueue(node.right);
+                    noNodesInNextLevel++;
+                }
+            }
+            noNodesInLevel = noNodesInNextLevel;
+        }
+        
+        return depth;
+    }
+}
