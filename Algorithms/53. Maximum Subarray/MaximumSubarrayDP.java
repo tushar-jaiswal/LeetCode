90735a0a06108adc4ebb0a4b3918aed3a888d5f2
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-07-17 07:00:40 -0500

    Created MaximumSubarrayDP.java
    Java Dynamic Programming solution for the Maximum Subarray problem.
    Problem Source: https://leetcode.com/problems/maximum-subarray/

---
+++ Algorithms/53. Maximum Subarray/MaximumSubarrayDP.java
@@ -0,0 +1,37 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/17/2016
+/*Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
+For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
+the contiguous subarray [4,-1,2,1] has the largest sum = 6.
+More practice:
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.*/
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        int p1=0, sum, maxP1=0, maxP2=0, maxSum;
+        
+        if(nums.length == 0)
+        {
+            return 0;
+        }
+        
+        sum = nums[0];
+        maxSum = nums[0];
+        for(int i = 1; i < nums.length; i++)
+        {
+            sum = Math.max(nums[i], sum + nums[i]);
+            if(sum == nums[i])
+            {
+                p1 = i;
+            }
+            
+            maxSum = Math.max(sum, maxSum);
+            if(maxSum == sum)
+            {
+                maxP1 = p1;
+                maxP2 = i;
+            }
+        }
+        
+        return maxSum;
+    }
+}
