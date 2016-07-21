c8910a62530d5662acfb336674bd08317bdb2031
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-07-20 22:27:36 -0500

    Created ValidPalindrome.java
    Java solution for the Valid Palindrome problem.
    Problem Source: https://leetcode.com/problems/valid-palindrome/

---
+++ Algorithms/125. Valid Palindrome/ValidPalindrome.java
@@ -0,0 +1,30 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/20/2016
+/*Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+For the purpose of this problem, we define empty string as valid palindrome.*/
+public class Solution {
+    public boolean isPalindrome(String s) {
+        for(int i = 0, j = s.length()-1; i < j; i++, j--)
+        {
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i)))
+            { i++; }
+            while(j > i && !Character.isLetterOrDigit(s.charAt(j)))
+            { j--; }
+            
+            if(i < j)
+            {
+                if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
