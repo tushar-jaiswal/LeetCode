51027f79f0e7c8540967ad3254885c5c1bfaa666
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-08-01 23:54:56 -0500

    Simplifying solution.
    Simplifying solution by getting rid of the GetChar() function.

--- Algorithms/168. Excel Sheet Column Title/ExcelSheetColumnTitle.cs
+++ Algorithms/168. Excel Sheet Column Title/ExcelSheetColumnTitle.cs
@@ -14,50 +14,19 @@ For example:
 public class Solution {
     public string ConvertToTitle(int n) {
         string result = "";
-        int rem;
+        
+        if(n <= 0)
+        {
+            throw new ArgumentOutOfRangeException("n", "Input must be greater than 0.");
+        }
         
         do
         {
             n = n - 1;
-            rem = n % 26;
-            result = GetChar(rem) + result;
+            result = (char) (n % 26 + 'A') + result;
             n = n / 26;
         }while(n != 0);
         
         return result;
-    }
-    
-    private char GetChar(int i)
-    {
-        switch(i) 
-        {
-            case 0: return 'A';
-            case 1: return 'B';
-            case 2: return 'C';
-            case 3: return 'D';
-            case 4: return 'E';
-            case 5: return 'F';
-            case 6: return 'G';
-            case 7: return 'H';
-            case 8: return 'I';
-            case 9: return 'J';
-            case 10: return 'K';
-            case 11: return 'L';
-            case 12: return 'M';
-            case 13: return 'N';
-            case 14: return 'O';
-            case 15: return 'P';
-            case 16: return 'Q';
-            case 17: return 'R';
-            case 18: return 'S';
-            case 19: return 'T';
-            case 20: return 'U';
-            case 21: return 'V';
-            case 22: return 'W';
-            case 23: return 'X';
-            case 24: return 'Y';
-            case 25: return 'Z';
-            default: return ' ';
-        }
     }
 }
