51f203f547d538dc35d40f9c7d933b9dfe37c0d9
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-08-09 18:17:56 -0500

    Created ReverseBits2.cs
    c# solution 2 for the ReverseBits problem.
    Problem Source: https://leetcode.com/problems/reverse-bits/

---
+++ Algorithms/190. Reverse Bits/ReverseBits2.cs
@@ -0,0 +1,17 @@
+//Author: Tushar Jaiswal
+//Creation Date: 08/05/2016
+
+/*Reverse bits of a given 32 bits unsigned integer.
+For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+Follow up: If this function is called many times, how would you optimize it?*/
+
+public class Solution {
+    public uint reverseBits(uint n) {
+        uint result = 0;
+        for(int i = 0; i < 32; i++)
+        {
+            result |= ((n >> i) & 1) << (31 - i); 
+        }
+        return result;
+    }
+}
