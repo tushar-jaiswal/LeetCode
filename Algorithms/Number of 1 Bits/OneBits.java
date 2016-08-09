94e94a538380f26b787f15726969931555fa73d6
Tushar Jaiswal <tjaiswal3@gatech.edu>
2016-08-09 18:25:20 -0500

    Created OneBits.java
    Java solution for the Number of 1 Bits problem.
    Problem Source: https://leetcode.com/problems/number-of-1-bits/

---
+++ Algorithms/Number of 1 Bits/OneBits.java
@@ -0,0 +1,18 @@
+//Author: Tushar Jaiswal
+//Creation Date: 08/09/2016
+
+/*Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+For example, the 32-bit integer '11' has binary representation 00000000000000000000000000001011, so the function should return 3.*/
+
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int weight = 0;
+        for(int i = 0; i < 32; i++)
+        {
+            if(((1 << i) & n) != 0)
+            { weight++; }
+        }
+        return weight;
+    }
+}
