12857ba99838cad0f1c3274ef59ab3b1b05a8574
Tushar Jaiswal <tjaiswal3@gatech.edu>
2017-02-20 19:08:32 -0600

    Created ReverseLinkedList2.cs
    C# solution 2 for the Reverse Linked List problem.
    Problem Source: https://leetcode.com/problems/reverse-linked-list/

---
+++ Algorithms/206. Reverse Linked List/ReverseLinkedList2.cs
@@ -0,0 +1,32 @@
+//Author: Tushar Jaiswal
+//Creation Date: 02/19/2017
+
+/*Reverse a singly linked list.
+Hint: A linked list can be reversed either iteratively or recursively. Could you implement both?*/
+
+//O(n) runtime complxity and O(1) space complexity, iterative solution
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode prev = null;
+        
+        while(curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        
+        return prev;
+    }
+}
