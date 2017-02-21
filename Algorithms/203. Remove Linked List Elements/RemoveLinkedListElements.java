14099de70c170feaf9ad536f035916907da8bacd
Tushar Jaiswal <tjaiswal3@gatech.edu>
2017-02-20 18:55:29 -0600

    Created RemoveLinkedListElements.java
    Java solution for the Remove Linked List Elements problem.
    Problem Source:
    https://leetcode.com/problems/remove-linked-list-elements/

---
+++ Algorithms/203. Remove Linked List Elements/RemoveLinkedListElements.java
@@ -0,0 +1,37 @@
+//Author: Tushar Jaiswal
+//Creation Date: 02/01/2017
+
+
+/*Remove all elements from a linked list of integers that have value val.
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while(head != null && head.val == val)
+        { head = head.next; }
+        
+        ListNode curr = head;
+        
+        while(curr != null && curr.next != null)
+        {
+            if(curr.next.val == val)
+            {
+                curr.next = curr.next.next;
+            }
+            else
+            { curr = curr.next; }
+        }
+        
+        return head;
+    }
+}
