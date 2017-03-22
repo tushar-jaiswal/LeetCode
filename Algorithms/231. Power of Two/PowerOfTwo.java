adf27581f29a983e1af72f6c64206d3e45ed5679
Tushar Jaiswal <tjaiswal3@gatech.edu>
2017-03-21 21:53:44 -0500

    Created PowerOfTwo.java
    Java solution for the Power of Two problem.
    Problem Source: https://leetcode.com/problems/power-of-two

---
+++ Algorithms/231. Power of Two/PowerOfTwo.java
@@ -0,0 +1,23 @@
+//Author: Tushar Jaiswal
+//Creation Date: 03/21/2017
+
+/*Given an integer, write a function to determine if it is a power of two.*/
+
+public class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 1) 
+        { return true; }
+        
+        while(n > 2)
+        {
+            int remainder = n % 2;
+            if(remainder == 1)
+            { return false; }
+            n /= 2;
+        }
+        if(n == 2)
+        { return true; }
+        else
+        { return false; }
+    }
+}
