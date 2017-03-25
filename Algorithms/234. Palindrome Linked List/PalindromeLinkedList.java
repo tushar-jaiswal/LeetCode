054a5f9c749516bec50888419cdf479ffc1c4405
Tushar <tjaiswal3@gatech.edu>
2017-03-25 15:39:25 -0500

    Created PalindromeLinkedList.java
    Java solution for the Palindrome Linked List problem.
    Problem Source: https://leetcode.com/problems/palindrome-linked-list/

---
+++ Algorithms/234. Palindrome Linked List/PalindromeLinkedList.java
@@ -0,0 +1,35 @@
+//Author: Tushar Jaiswal
+//Creation Date: 03/25/2017
+
+/*Given a singly linked list, determine if it is a palindrome.
+Follow up: Could you do it in O(n) time and O(1) space?*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isPalindrome(ListNode head) {
+        int a = 0, b = 0, i = 0;
+        while(head != null)
+        {
+            a = 10 * a + head.val;
+            if(i == 0)
+            {
+                b = head.val + b;
+                i = 1; 
+            }
+            else
+            {
+                i *= 10;
+                b = i * head.val + b;
+            }
+            head = head.next;
+        }
+        return a == b;
+    }
+}
