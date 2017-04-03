792a1d59b5cafbded4f0d1875bad83e14561cef7
Tushar <tjaiswal3@gatech.edu>
2017-04-02 21:19:23 -0500

    Updated variable names to better match ugly factors

--- Algorithms/264. Ugly Number II/UglyNumberII.java
+++ Algorithms/264. Ugly Number II/UglyNumberII.java
@@ -7,24 +7,24 @@ Note that 1 is typically treated as an ugly number, and n does not exceed 1690.*
 
 public class Solution {
     public int nthUglyNumber(int n) {
-        int l1, l2, l3;
+        int l2, l3, l5;
         int[] ugly = new int[1690];
         ugly[0] = 1;
-        l1 = l2 = l3 = 0;
+        l2 = l3 = l5 = 0;
         for(int i = 1; i < n; i++)
         {
-            ugly[i] = Math.min(ugly[l1] * 2, Math.min(ugly[l2] * 3, ugly[l3] * 5));
-            if(ugly[i] == ugly[l1] * 2)
-            {
-                l1++;
-            }
-            if(ugly[i] == ugly[l2] * 3)
+            ugly[i] = Math.min(ugly[l2] * 2, Math.min(ugly[l3] * 3, ugly[l5] * 5));
+            if(ugly[i] == ugly[l2] * 2)
             {
                 l2++;
             }
-            if(ugly[i] == ugly[l3] * 5)
+            if(ugly[i] == ugly[l3] * 3)
             {
                 l3++;
+            }
+            if(ugly[i] == ugly[l5] * 5)
+            {
+                l5++;
             }
         }
         return ugly[n-1];
