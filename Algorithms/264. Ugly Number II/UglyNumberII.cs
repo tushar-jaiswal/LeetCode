fd30a5ae2906203840149b29f2515644904811c2
Tushar <tjaiswal3@gatech.edu>
2017-04-02 21:12:28 -0500

    Created UglyNumberII.cs
    C# solution for the Ugly Number II problem.
    Problem Source: https://leetcode.com/problems/ugly-number-ii

---
+++ Algorithms/264. Ugly Number II/UglyNumberII.cs
@@ -0,0 +1,32 @@
+//Author: Tushar Jaiswal
+//Creation Date: 04/02/2017
+
+/*Write a program to find the n-th ugly number.
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+Note that 1 is typically treated as an ugly number, and n does not exceed 1690.*/
+
+public class Solution {
+    public int NthUglyNumber(int n) {
+        int l1, l2, l3;
+        int[] ugly = new int[1690];
+        ugly[0] = 1;
+        l1 = l2 = l3 = 0;
+        for(int i = 1; i < n; i++)
+        {
+            ugly[i] = Math.Min(ugly[l1] * 2, Math.Min(ugly[l2] * 3, ugly[l3] * 5));
+            if(ugly[i] == ugly[l1] * 2)
+            {
+                l1++;
+            }
+            if(ugly[i] == ugly[l2] * 3)
+            {
+                l2++;
+            }
+            if(ugly[i] == ugly[l3] * 5)
+            {
+                l3++;
+            }
+        }
+        return ugly[n-1];
+    }
+}
