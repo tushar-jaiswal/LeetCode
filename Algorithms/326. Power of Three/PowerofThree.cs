c454e16dc7333aafbd6f32460e3dcd39ee5802ed
Tushar Jaiswal <tjaiswal3@gatech.edu>
2017-04-22 20:49:59 -0500

    Created PowerofTHree.cs
    C# solution for the Power of Three problem.
    Problem Source: https://leetcode.com/problems/power-of-three

---
+++ Algorithms/326. Power of Three/PowerofThree.cs
@@ -0,0 +1,19 @@
+//Author: Tushar Jaiswal
+//Creation Date: 04/22/2017
+
+/*Given an integer, write a function to determine if it is a power of three.
+Follow up: Could you do it without using any loop / recursion?*/
+
+public class Solution {
+    public bool IsPowerOfThree(int n) {
+        if(n == 0)
+        { return false; }
+        if(n == 1)
+        { return true; }
+        while(n % 3 == 0 && n != 3)
+        {
+            n /= 3;
+        }
+        return (n % 3 == 0) ? true : false;
+    }
+}
