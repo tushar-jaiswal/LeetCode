70c45fb7437541c5c47b46b33a0a0b630acb6ea2
Tushar Jaiswal <tjaiswal3@gatech.edu>
2017-06-14 18:53:59 -0500

    Created UniqueChar.java
    Java solution for the First Unique Character in a String problem.
    Problem Source:
    https://leetcode.com/problems/first-unique-character-in-a-string

---
+++ Algorithms/387. First Unique Character in a String/UniqueChar.java
@@ -0,0 +1,35 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/13/2017
+
+/*Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+s = "leetcode", return 0.
+s = "loveleetcode", return 2.*/
+
+public class Solution {
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character, Integer> chars = new LinkedHashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(chars.containsKey(c))
+            { 
+                chars.put(c, -1);
+            }
+            else
+            {
+                chars.put(c, i);
+            }
+        }
+        
+        for(Integer value : chars.values())
+        {
+            if(value != -1)
+            { 
+                return value; 
+            }
+        }
+        return -1;
+    }
+}
