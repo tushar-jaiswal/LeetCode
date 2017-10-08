5ab1b3979a6311d53e2152185ca1eadb5ee2bb25
Tushar Jaiswal <tjaiswal3@gatech.edu>
2017-10-08 14:05:31 -0500

    Created FindDisappearedNumbers.cs
    C# solution for the Find All Numbers Disappeared in an Array problem.
    Problem Source: https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array

---
+++ Algorithms/448. Find All Numbers Disappeared in an Array/FindDisappearedNumbers.cs
@@ -0,0 +1,31 @@
+//Author: Tushar Jaiswal
+//Creation Date: 10/08/2017
+
+/*Given an array of integers where 1 = a[i] = n (n = size of array), some elements appear twice and others appear once.
+Find all the elements of [1, n] inclusive that do not appear in this array.
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+Example:
+Input: [4,3,2,7,8,2,3,1]
+Output: [5,6]*/
+
+public class Solution {
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        List<int> result = new List<int>();
+        for(int i = 0; i < nums.Length; i++)
+        {
+            int index = Math.Abs(nums[i]) - 1;
+            nums[index] = -Math.Abs(nums[index]);
+        }
+        
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] > 0)
+            {
+                result.Add(i + 1);
+            }
+        }
+       
+        return result;
+    }
+}
