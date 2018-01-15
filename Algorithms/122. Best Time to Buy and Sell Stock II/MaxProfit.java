cd6b3f27f2a15ab82bdfb830e63d4c1cc9752d69
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-01-14 18:32:29 -0600

    Created MaxProfit.java
    Java solution for the Best Time to Buy and Sell Stock II problem.
    Problem Source: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii

---
+++ Algorithms/122. Best Time to Buy and Sell Stock II/MaxProfit.java
@@ -0,0 +1,39 @@
+//Author: Tushar Jaiswal
+//Creation Date: 01/14/2018
+
+/*Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int totalProfit = 0, min = -1, currentProfit = -1;
+        if(prices.length != 0)
+        {
+            min = 0; 
+        }
+        for(int i = 1; i < prices.length; i++)
+        {
+            int profit = prices[i] - prices[min];
+            
+            if(profit > 0 && profit >= currentProfit)
+            { 
+                currentProfit = profit; 
+                if(i == prices.length - 1)
+                {
+                    totalProfit += currentProfit;
+                }
+            }
+            else 
+            {
+                min = i;
+                if(currentProfit != -1 && currentProfit > profit)
+                {
+                    totalProfit += currentProfit;
+                    currentProfit = -1;
+                }
+            }
+        }
+        return totalProfit;
+    }
+}
