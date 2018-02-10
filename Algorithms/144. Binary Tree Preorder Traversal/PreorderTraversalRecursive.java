c0640e048f74e85393dc3c8754dd61b8f6d9840e
Tushar <tjaiswal3@gatech.edu>
2018-02-10 17:57:26 -0600

    Created PreorderTraversalRecursive.java
    Java recursive solution for the Binary Tree Preorder Traversal problem.
    Problem Source:
    https://leetcode.com/problems/binary-tree-preorder-traversal

---
+++ Algorithms/144. Binary Tree Preorder Traversal/PreorderTraversalRecursive.java
@@ -0,0 +1,36 @@
+//Author: Tushar Jaiswal
+//Creation Date: 02/10/2018
+
+/*Given a binary tree, return the preorder traversal of its nodes' values.
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Note: Recursive solution is trivial, could you do it iteratively?*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root != null)            
+        {
+            list.add(root.val);
+            list.addAll(preorderTraversal(root.left));
+            list.addAll(preorderTraversal(root.right));
+        }
+        return list;
+    }
+}
