8038335b2176ec290351e8d78302ab0a93a6a39f
Tushar <tjaiswal3@gatech.edu>
2018-02-10 18:43:42 -0600

    Created PreorderTraversalIterative.cs
    C# iterative solution for the Binary Tree Preorder Traversal problem.
    Problem Source:
    https://leetcode.com/problems/binary-tree-preorder-traversal

---
+++ Algorithms/144. Binary Tree Preorder Traversal/PreorderTraversalIterative.cs
@@ -0,0 +1,45 @@
+//Author: Tushar Jaiswal
+//Creation Date: 02/10/2018
+
+/*Given a binary tree, return the preorder traversal of its nodes' values.
+For example:
+Given binary tree [1,null,2,3],
+   1
+    \
+     2
+    /
+   3
+return [1,2,3].
+
+Note: Recursive solution is trivial, could you do it iteratively?*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<int> PreorderTraversal(TreeNode root) {
+        List<int> list = new List<int>();
+        Stack<TreeNode> nodes = new Stack<TreeNode>();
+        
+        if(root != null)
+        { nodes.Push(root); }
+        
+        while(nodes.Count != 0)            
+        {
+            TreeNode curr = nodes.Pop();
+            list.Add(curr.val);
+            
+            if(curr.right != null)
+            { nodes.Push(curr.right); }
+            if(curr.left != null)
+            { nodes.Push(curr.left); }
+        }
+        return list;
+    }
+}
