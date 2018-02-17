b055a1782987266eb4093e795d6443739ead751f
Tushar <tjaiswal3@gatech.edu>
2018-02-17 11:45:55 -0600

    Created MaxDepthPostOrder.cs
    C# postorder traversal solution for the Maximum Depth of Binary Tree
    problem.
    Problem Source:
    https://leetcode.com/problems/maximum-depth-of-binary-tree

---
+++ Algorithms/104. Maximum Depth of Binary Tree/MaxDepthPostOrder.cs
@@ -0,0 +1,32 @@
+//Author: Tushar Jaiswal
+//Creation Date: 02/17/2015
+
+/*Given a binary tree, find its maximum depth.
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        if(root == null)
+        { return 0; }
+        int leftDepth = MaxDepth(root.left);
+        int rightDepth = MaxDepth(root.right);
+        return Math.Max(leftDepth, rightDepth) + 1;
+    }
+}
