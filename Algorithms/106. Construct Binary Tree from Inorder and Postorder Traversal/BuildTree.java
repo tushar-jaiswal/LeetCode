1f5e58e6d406595279dc79dd88d964507402db49
Tushar <tjaiswal3@gatech.edu>
2018-03-10 19:39:11 -0600

    Created BuildTree.java
    Java solution for the Construct Binary Tree from Inorder and Postorder
    Traversal problem.
    Problem Source:
    https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal

---
+++ Algorithms/106. Construct Binary Tree from Inorder and Postorder Traversal/BuildTree.java
@@ -0,0 +1,69 @@
+//Author: Tushar Jaiswal
+//Creation Date: 03/10/2018
+
+/*Given inorder and postorder traversal of a tree, construct the binary tree.
+Note: You may assume that duplicates do not exist in the tree.
+
+For example, given
+inorder = [9,3,15,20,7]
+postorder = [9,15,7,20,3]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        int length =  postorder.length;
+        if (length == 0)
+        { return null; }
+        
+        int rootVal = postorder[length - 1];
+        TreeNode root = new TreeNode(rootVal);
+        
+        int leftLength = 0, rightLength = 0;
+        for(int i = 0; i < length; i++)
+        {
+            if(inorder[i] == rootVal)
+            {
+                leftLength = i;
+                rightLength = length - (i + 1);
+            }
+        }
+        
+        int[] rightInorder = new int[rightLength];
+        int[] rightPostorder = new int[rightLength];
+        int[] leftInorder = new int[leftLength];
+        int[] leftPostorder = new int[leftLength];
+        for(int i = 0; i < leftLength || i < rightLength; i++)
+        {
+            if(i < leftLength)
+            {
+                leftInorder[i] = inorder[i];
+                leftPostorder[i] = postorder[i];
+            }
+            if(i < rightLength)
+            {
+                rightInorder[i] = inorder[leftLength + 1 + i];
+                rightPostorder[i] = postorder[leftLength + i];
+            }
+        }
+        
+        root.right = buildTree(rightInorder, rightPostorder);
+        root.left = buildTree(leftInorder, leftPostorder);
+        
+        return root;
+    }
+}
