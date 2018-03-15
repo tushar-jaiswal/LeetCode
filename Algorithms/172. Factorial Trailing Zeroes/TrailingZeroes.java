899575a5d9eab30a50507e8cdbb5563213d0db3f
Tushar <tjaiswal3@gatech.edu>
2018-03-15 08:05:21 -0500

    Created TrailingZeroes.java
    Java solution for the Trailing Zeroes problem.
    Problem Source: https://leetcode.com/problems/factorial-trailing-zeroes

---
+++ Algorithms/172. Factorial Trailing Zeroes/TrailingZeroes.java
@@ -0,0 +1,17 @@
+//Author: Tushar Jaiswal
+//Creation Date: 03/15/2018
+
+/*Given an integer n, return the number of trailing zeroes in n!.
+Note: Your solution should be in logarithmic time complexity.*/
+
+class Solution {
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while(n != 0)
+        {
+            n /= 5;
+            result += n;
+        }
+        return result;
+    }
+}
