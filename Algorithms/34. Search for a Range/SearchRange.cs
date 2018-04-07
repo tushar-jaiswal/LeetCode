bec8dc87232959cff80684d6a0c7559535eed3f1
Tushar <tjaiswal3@gatech.edu>
2018-04-07 18:37:47 -0500

    Created SearchRange.cs
    C# solution for the Search for a Range problem.
    Problem Source: https://leetcode.com/problems/search-for-a-range

---
+++ Algorithms/34. Search for a Range/SearchRange.cs
@@ -0,0 +1,67 @@
+//Author: Tushar Jaiswal
+//Creation Date: 04/01/2018
+
+/*Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+Your algorithm's runtime complexity must be in the order of O(log n).
+If the target is not found in the array, return [-1, -1].
+
+For example,
+Given [5, 7, 7, 8, 8, 10] and target value 8,
+return [3, 4].*/
+
+public class Solution {
+    public int[] SearchRange(int[] nums, int target) {
+        int[] range = new int[2];
+        range[0] = binarySearchLeft(nums, target);
+        range[1] = binarySearchRight(nums, target);
+        return range;
+    }
+    
+    public int binarySearchLeft(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while(left + 1 < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+            {
+                if(nums[mid - 1] != target)
+                { return mid; }
+                right = mid - 1;
+            }
+            else if(target < nums[mid])
+            { right = mid - 1; }
+            else
+            { left = mid + 1; }
+        }
+        if(left < nums.Length && nums[left] == target)
+        { return left; }
+        if(right >=0 && nums[right] == target)
+        { return right; }
+        return -1;
+    }
+    
+    public int binarySearchRight(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+        while(left + 1 < right)
+        {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+            {
+                if(nums[mid + 1] != target)
+                { return mid; }
+                left = mid + 1;
+            }
+            else if(target < nums[mid])
+            { right = mid - 1; }
+            else
+            { left = mid + 1; }
+        }
+        if(right >=0 && nums[right] == target)
+        { return right; }
+        if(left < nums.Length && nums[left] == target)
+        { return left; }
+        return -1;
+    }
+}
