10b7d5d4e9480a2bc3296ab3684e667693cf8ec9
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-01 10:00:29 -0500

    Created NextGreatestLetter.cs
    C# solution for the Find Smallest Letter Greater Than Target problem.
    Problem Source: https://leetcode.com/problems/find-smallest-letter-greater-than-target

---
+++ Algorithms/744. Find Smallest Letter Greater Than Target/NextGreatestLetter.cs
@@ -0,0 +1,65 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/01/2018
+
+/*Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+Examples:
+Input:
+letters = ["c", "f", "j"]
+target = "a"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "c"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "d"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "g"
+Output: "j"
+
+Input:
+letters = ["c", "f", "j"]
+target = "j"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "k"
+Output: "c"*/
+
+public class Solution {
+    public char NextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.Length - 1, mid = -1;
+        
+        while(low <= high)
+        {
+            mid = low + (high - low) / 2;
+            if(letters[mid] == target)
+            { 
+                if(mid + 1 < letters.Length && letters[mid + 1] == target)
+                { low = mid + 1; }
+                else
+                { break; }
+            }
+            else if(letters[mid] > target)
+            { high = mid - 1; }
+            else if(mid + 1 < letters.Length && letters[mid + 1] > target)
+            { break; }
+            else
+            { low = mid + 1; }
+        }
+        if(high < 0)
+        { return letters[0]; }
+        if(mid == letters.Length - 1)
+        { return letters[0]; }
+        return letters[mid + 1];
+    }
+}
