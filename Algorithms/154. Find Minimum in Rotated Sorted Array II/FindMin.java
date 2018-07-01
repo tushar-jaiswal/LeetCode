737c0ce8c0dbb7a8d1ecd133bc18ba793899d634
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-01 17:34:22 -0500

    Corrected problem statement

--- Algorithms/154. Find Minimum in Rotated Sorted Array II/FindMin.java
+++ Algorithms/154. Find Minimum in Rotated Sorted Array II/FindMin.java
@@ -2,9 +2,19 @@
 //Creation Date: 07/01/2018
 
 /*Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+(i.e.,  [0,1,2,4,5,6,7] might become  [4,5,6,7,0,1,2]).
 Find the minimum element.
-You may assume no duplicate exists in the array.*/
+The array may contain duplicates.
+
+Example 1:
+Input: [1,3,5]
+Output: 1
+
+Example 2:
+Input: [2,2,2,0,1]
+Output: 0
+
+Note: This is a follow up problem to Find Minimum in Rotated Sorted Array. Would allow duplicates affect the run-time complexity? How and why?*/
 
 class Solution {
     public int findMin(int[] nums) {
