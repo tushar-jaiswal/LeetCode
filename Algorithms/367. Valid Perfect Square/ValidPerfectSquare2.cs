216b9491f5687a2bcf2d76c76fac96cf4acb7c6f
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-06-30 20:36:04 -0500

    Created ValidPerfectSquare.cs
    C# Binary Search solution for the Valid Perfect Square problem.
    Problem Source: https://leetcode.com/problems/valid-perfect-square

---
+++ Algorithms/367. Valid Perfect Square/ValidPerfectSquare2.cs
@@ -0,0 +1,30 @@
+//Author: Tushar Jaiswal
+//Creation Date: 06/30/2018
+
+/*Given a positive integer num, write a function which returns True if num is a perfect square else False.
+Note: Do not use any built-in library function such as sqrt.
+
+Example 1:
+Input: 16
+Returns: True
+
+Example 2:
+Input: 14
+Returns: False*/
+
+public class Solution {
+    public bool IsPerfectSquare(int num) {
+        long low = 1, high = num / 2 + 1;
+        while(low <= high)
+        {
+            long mid = low + (high - low) / 2;
+            if(mid * mid == num)
+            { return true; }
+            else if(mid * mid > num)
+            { high = mid - 1; }
+            else
+            { low = mid + 1; }
+        }
+        return false;
+    }
+}
