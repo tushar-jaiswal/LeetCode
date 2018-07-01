e34070e775d05e9c2406bbe1d9b2060948075320
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-01 10:28:23 -0500

    Created NextGreatestLetter2.java
    Java solution for the Find Smallest Letter Greater Than Target problem.
    Problem Source: https://leetcode.com/problems/find-smallest-letter-greater-than-target

---
+++ Algorithms/744. Find Smallest Letter Greater Than Target/NextGreatestLetter2.java
@@ -0,0 +1,52 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/01/2018
+
+/*Given a list of sorted characters letters containing only lowercase letters, and given a target letter target, find the smallest element in the list that is larger than the given target.
+Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+
+Examples:
+Input:
+letters = ["c", "f", "j"]
+target = "a"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "c"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "d"
+Output: "f"
+
+Input:
+letters = ["c", "f", "j"]
+target = "g"
+Output: "j"
+
+Input:
+letters = ["c", "f", "j"]
+target = "j"
+Output: "c"
+
+Input:
+letters = ["c", "f", "j"]
+target = "k"
+Output: "c"*/
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length;
+        
+        while(low < high)
+        {
+            int mid = low + (high - low) / 2;
+            if(letters[mid] <= target)
+            { low = mid + 1; }
+            else
+            { high = mid; }
+        }
+        return letters[low % letters.length];
+    }
+}
