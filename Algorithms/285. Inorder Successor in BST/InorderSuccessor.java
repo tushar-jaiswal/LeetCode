e632e233bb559008a87fd7fbad648c92a27da8c4
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-07 16:36:57 -0500

    Created InorderSuccessor.java
    Java solution for the Inorder Successor in BST problem.
    Problem Source: https://leetcode.com/problems/inorder-successor-in-bst

---
+++ Algorithms/285. Inorder Successor in BST/InorderSuccessor.java
@@ -0,0 +1,27 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/07/2018
+
+/*Given a binary search tree and a node in it, find the in-order successor of that node in the BST.*/
+
+class Solution {
+    public TreeNode prevNode;
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode node)
+    {
+        if(root != null)
+		{
+			TreeNode result = inorderSuccessor(root.left, node);
+			if(result != null)
+			{ return result; }
+			
+			if(prevNode != null && prevNode.val == node.val)
+			{ return root; }
+			else
+			{ prevNode = root; }
+			
+			result = inorderSuccessor(root.right, node);
+			if(result != null)
+			{ return result; }
+		}
+		return null;
+    }
+}
