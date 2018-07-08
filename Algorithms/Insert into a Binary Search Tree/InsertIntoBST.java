bf12ea0e266ae1813567c20493821bf974051328
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-08 11:37:50 -0500

    Created InsertIntoBST.java
    Java solution for the Insert into a Binary Search Tree problem.
    Problem Source: https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/141/basic-operations-in-a-bst/1003/

---
+++ Algorithms/Insert into a Binary Search Tree/InsertIntoBST.java
@@ -0,0 +1,65 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/08/2018
+
+/*Given the root node of a binary search tree (BST) and a value to be inserted into the tree, insert the value into the BST. Return the root node of the BST after the insertion. It is guaranteed that the new value does not exist in the original BST.
+Note that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. You can return any of them.
+
+For example, 
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+And the value to insert: 5
+You can return this binary search tree:
+         4
+       /   \
+      2     7
+     / \   /
+    1   3 5
+This tree is also valid:
+         5
+       /   \
+      2     7
+     / \   
+    1   3
+         \
+          4*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        TreeNode curr = root;
+        while(true)
+        {
+            if(val < curr.val)
+            {
+                if(curr.left == null)
+                {
+                    curr.left = new TreeNode(val);
+                    break;
+                }
+                curr = curr.left;
+            }
+            else
+            {
+                if(curr.right == null)
+                {
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+                curr = curr.right;
+            }
+        }
+        return root;
+    }
+}
