76069ea0d3d9d03d7c11146be8d05a3c47db7133
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-07 22:37:06 -0500

    Created InorderSuccessor2.cs
    C# solution for the Inorder Successor in BST problem.
    Problem Source: https://leetcode.com/problems/inorder-successor-in-bst

---
+++ Algorithms/285. Inorder Successor in BST/InorderSuccessor2.cs
@@ -0,0 +1,35 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/07/2018
+
+/*Given a binary search tree and a node in it, find the in-order successor of that node in the BST.*/
+
+public class Solution
+{
+	public TreeNode InorderSuccessor(TreeNode root, TreeNode node)
+	{
+		if(root != null)
+		{
+			if(node.right != null)
+			{
+				node = node.right;
+				while(node.left != null)
+				{ node = node.left; }
+				return node;
+			}
+			
+			TreeNode result = null;
+			while(root != null && node.val != root.val)
+			{
+				if(node.val < root.val)
+				{
+					result = root;
+					root = root.left;
+				}
+				else
+				{ root = root.right; }
+			}
+			return result;
+		}
+		return null;
+	}
+}
