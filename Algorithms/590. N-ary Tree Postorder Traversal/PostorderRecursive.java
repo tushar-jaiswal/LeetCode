e3c25d573f79275e706d40e940a5dc81c1356e31
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-07-29 19:07:34 -0500

    Created PostorderRecursive.java
    Java recursive solution for the N-ary Tree Postorder Traversal problem.
    Problem Source: https://leetcode.com/problems/n-ary-tree-postorder-traversal

---
+++ Algorithms/590. N-ary Tree Postorder Traversal/PostorderRecursive.java
@@ -0,0 +1,36 @@
+//Author: Tushar Jaiswal
+//Creation Date: 07/29/2016
+
+/*Given an n-ary tree, return the postorder traversal of its nodes' values.
+For example, given a 3-ary tree:
+Return its postorder traversal as: [5,6,3,2,4,1].
+Note: Recursive solution is trivial, could you do it iteratively?*/
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if(root != null)
+        {
+            for(Node child : root.children)
+            {
+                list.addAll(postorder(child));
+            }
+            list.add(root.val);
+        }
+        return list;
+    }
+}
