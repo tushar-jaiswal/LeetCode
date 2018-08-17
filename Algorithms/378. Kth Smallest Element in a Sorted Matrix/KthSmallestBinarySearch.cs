eab7fac2a9689c1a67ccccdaf63e74aaf824932d
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-08-16 21:10:50 -0500

    Created KthSmallestBinarySearch.cs
    C# binary search solution for the Kth Smallest Element in a Sorted Matrix problem.
    Problem Source: https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix

---
+++ Algorithms/378. Kth Smallest Element in a Sorted Matrix/KthSmallestBinarySearch.cs
@@ -0,0 +1,45 @@
+//Author: Tushar Jaiswal
+//Creation Date: 08/16/2018
+
+/*Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+return 13.
+Note: You may assume k is always valid, 1 = k = n2.*/
+
+public class Solution {
+    public int KthSmallest(int[,] matrix, int k) {
+        int n = matrix.GetLength(0);
+        int low = matrix[0, 0], high = matrix[n - 1, n - 1];
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int maxNum = low;
+            int count = 0;
+            for(int i = 0, j = n - 1; i < n; i++)
+            {
+                while(j >= 0 && matrix[i, j] > mid) 
+                { j--; }
+                if(j >= 0)
+                { 
+                    count += j + 1;
+                    maxNum = Math.Max(maxNum, matrix[i, j]); 
+                }
+            }
+            if(count == k)
+            { return maxNum; }
+            if(count < k)
+            { low = mid + 1; }
+            else
+            { high = mid - 1; }
+        }
+        return low;
+    }
+}
