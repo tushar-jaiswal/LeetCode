9d783c1ae07471eb28673f67febcb39ac0da5f25
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-08-23 23:55:29 -0500

    Created Divide.java
    Java solution for the Divide Two Integers problem.
    Problem Source: https://leetcode.com/problems/divide-two-integers

---
+++ Algorithms/29. Divide Two Integers/Divide.java
@@ -0,0 +1,52 @@
+//Author: Tushar Jaiswal
+//Creation Date: 08/23/2018
+
+/*Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+Return the quotient after dividing dividend by divisor. The integer division should truncate toward zero.
+
+Example 1:
+Input: dividend = 10, divisor = 3
+Output: 3
+
+Example 2:
+Input: dividend = 7, divisor = -3
+Output: -2
+
+Note:
+Both dividend and divisor will be 32-bit signed integers. The divisor will never be 0. Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [-231,  231 - 1]. For the purpose of this problem, assume that your function returns 231 - 1 when the division result overflows.*/
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if(dividend == Integer.MIN_VALUE && divisor == -1)
+        { return Integer.MAX_VALUE; }
+        
+        int quotient = 0;
+        boolean isNegative = false;
+        if((dividend >= 0 && divisor < 0) || (dividend < 0 && divisor > 0))
+        { isNegative = true; }
+        long lDivisor = Math.abs((long)divisor);
+        long lDividend = Math.abs((long)dividend);
+        
+        long curr = lDivisor;
+        long divid = lDividend;
+        int count = 1;
+        while(lDivisor <= divid)
+        {
+            if(curr < divid)
+            {
+                curr += curr;
+                count += count;
+            }
+            if(curr > divid)
+            {
+                curr = lDivisor;
+                count = 1;
+            }
+            divid -= curr;
+            quotient += count;
+        }
+        if(isNegative)
+        { quotient = 0 - quotient; }
+        return quotient;
+    }
+}
