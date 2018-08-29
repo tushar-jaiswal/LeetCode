ab42102e8994f0de22f18f6a0bf5efe4a8b3f5d1
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-08-28 20:43:51 -0500

    Created CloneGraphDFSIterative.java
    Java iterative DFS solution for the Clone Graph problem.
    Problem Source: https://leetcode.com/problems/clone-grap

---
+++ Algorithms/133. Clone Graph/CloneGraphDFSIterative.java
@@ -0,0 +1,58 @@
+//Author: Tushar Jaiswal
+//Creation Date: 08/28/2018
+
+/*Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+
+OJ's undirected graph serialization: Nodes are labeled uniquely.
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/*/
+
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        UndirectedGraphNode clone = null;
+        if(node != null)
+        {
+            HashMap<Integer, UndirectedGraphNode> nodes = new HashMap<Integer, UndirectedGraphNode>();
+            nodes.put(node.label, new UndirectedGraphNode(node.label));
+            clone = nodes.get(node.label);
+            Deque<UndirectedGraphNode> stack = new ArrayDeque<UndirectedGraphNode>();
+            stack.addFirst(node);
+            while(!stack.isEmpty())
+            {
+                UndirectedGraphNode curr = stack.removeFirst();
+                UndirectedGraphNode currClone = nodes.get(curr.label);
+                for(UndirectedGraphNode neighbor : curr.neighbors)
+                {
+                    if(!nodes.containsKey(neighbor.label))
+                    {
+                        nodes.put(neighbor.label, new UndirectedGraphNode(neighbor.label));
+                        stack.addFirst(neighbor);
+                    }
+                    currClone.neighbors.add(nodes.get(neighbor.label));
+                }
+            }
+        }
+        return clone;
+    }
+}
