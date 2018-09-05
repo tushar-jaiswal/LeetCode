15a4723e70f16b06a4c1f1793436bec271ff9d25
Tushar Jaiswal <tjaiswal3@gatech.edu>
2018-09-04 22:49:20 -0500

    Created FindMinHeightTrees.java
    Java solution for the Minimum Height Trees problem.
    Problem Source: https://leetcode.com/problems/minimum-height-trees

---
+++ Algorithms/310. Minimum Height Trees/FindMinHeightTrees.java
@@ -0,0 +1,81 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/04/2018
+
+/*For a undirected graph with tree characteristics, we can choose any node as the root. The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs). Given such a graph, write a function to find all the MHTs and return a list of their root labels.
+Format: The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number n and a list of undirected edges (each edge is a pair of labels).
+You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+Example 1 :
+Input: n = 4, edges = [[1, 0], [1, 2], [1, 3]]
+        0
+        |
+        1
+       / \
+      2   3 
+Output: [1]
+
+Example 2 :
+Input: n = 6, edges = [[0, 3], [1, 3], [2, 3], [4, 3], [5, 4]]
+     0  1  2
+      \ | /
+        3
+        |
+        4
+        |
+        5 
+Output: [3, 4]
+
+Note:
+According to the definition of tree on Wikipedia: “a tree is an undirected graph in which any two vertices are connected by exactly one path. In other words, any connected graph without simple cycles is a tree.”
+The height of a rooted tree is the number of edges on the longest downward path between the root and a leaf.*/
+
+class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        HashMap<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+        int[] degree = new int[n];
+        
+        for(int i = 0; i < edges.length; i++)
+        {
+            if(!graph.containsKey(edges[i][0]))
+            { graph.put(edges[i][0], new ArrayList<Integer>()); }
+            if(!graph.containsKey(edges[i][1]))
+            { graph.put(edges[i][1], new ArrayList<Integer>()); }
+            graph.get(edges[i][0]).add(edges[i][1]);
+            graph.get(edges[i][1]).add(edges[i][0]);
+            degree[edges[i][0]]++;
+            degree[edges[i][1]]++;
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
+        for(int i = 0; i < n; i++)
+        { 
+            if(degree[i] == 1)
+            { queue.offer(i); }
+        }
+        while(!queue.isEmpty())
+        {
+            if(n <= 2)
+            {
+                while(!queue.isEmpty())
+                { result.add(queue.poll()); }
+                break;
+            }
+            int size = queue.size();
+            for(int i = 0; i < size; i++)
+            {
+                int node = queue.poll();
+                for(int curr : graph.get(node))
+                {
+                    degree[curr]--;
+                    if(degree[curr] == 1)
+                    { queue.offer(curr); }
+                }
+            }
+            n -= size;
+        }
+        if(result.size() == 0)
+        { result.add(0); }
+        return result;
+    }
+}
