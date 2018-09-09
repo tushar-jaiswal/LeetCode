e5a66a3388c98d6b3e79fc4d28de511f6489cb4a
Tushar <tjaiswal3@gatech.edu>
2018-09-09 12:59:28 -0500

    Created ShortestToChar.cs
    C# BFS solution for the Shortest Distance to a Character problem.
    Problem Source:
    https://leetcode.com/problems/shortest-distance-to-a-character

---
+++ Algorithms/821. Shortest Distance to a Character/ShortestToChar.cs
@@ -0,0 +1,46 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/09/2018
+
+/*Given a string S and a character C, return an array of integers representing the shortest distance from the character C in the string.
+
+Example 1:
+Input: S = "loveleetcode", C = 'e'
+Output: [3, 2, 1, 0, 1, 0, 0, 1, 2, 2, 1, 0]
+ 
+Note:
+S string length is in [1, 10000].
+C is a single character, and guaranteed to be in string S.
+All letters in S and C are lowercase.*/
+
+public class Solution {
+    public int[] ShortestToChar(string S, char C) {
+        int len = S.Length;
+        int[] result = new int[len];
+        for(int i = 0; i < len; i++)
+        { result[i] = -1; }
+        Queue<int> queue = new Queue<int>();
+        for(int i = 0; i < len; i++)
+        {
+            if(S[i] == C)
+            {
+                result[i] = 0; 
+                queue.Enqueue(i);
+            }
+        }
+        while(queue.Count != 0)
+        {
+            int i = queue.Dequeue();
+            if(i - 1 >= 0 && result[i - 1] == -1)
+            {
+                result[i - 1] = result[i] + 1;
+                queue.Enqueue(i - 1);
+            }
+            if(i + 1 < len && result[i + 1] == -1)
+            {
+                result[i + 1] = result[i] + 1;
+                queue.Enqueue(i + 1);
+            }
+        }
+        return result;
+    }
+}
