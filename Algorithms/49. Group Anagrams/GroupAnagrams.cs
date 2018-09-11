37613849775466780c503aa4944d6f4ad18a6693
Tushar <tjaiswal3@gatech.edu>
2018-09-11 18:54:21 -0500

    Created GroupAnagrams.cs
    C# solution for the Group Anagrams problem.
    Problem Source: https://leetcode.com/problems/group-anagrams

---
+++ Algorithms/49. Group Anagrams/GroupAnagrams.cs
@@ -0,0 +1,36 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/11/2018
+
+/*Given an array of strings, group anagrams together.
+
+Example:
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+
+Note:
+All inputs will be in lowercase.
+The order of your output does not matter.*/
+
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+        IList<IList<string>> result = new List<IList<string>>();
+        foreach(string str in strs)
+        {
+            char[] arr = str.ToArray();
+            Array.Sort(arr);
+            string sorted = new string(arr);
+            if(!dict.ContainsKey(sorted))
+            { dict[sorted] = new List<string>();  }
+            dict[sorted].Add(str);
+        }
+        foreach(List<string> list in dict.Values)
+        { result.Add(list); }
+        return result;
+    }
+}
