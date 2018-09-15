63bf444346939f7ec348fb29a1246a658af244d4
Tushar <tjaiswal3@gatech.edu>
2018-09-15 14:38:52 -0500

    Created CanJumpGreedy.cs
    C# optimized greedy solution for the Jump Game problem.
    Problem Source: https://leetcode.com/problems/jump-game

---
+++ Algorithms/55. Jump Game/CanJumpGreedy.cs
@@ -0,0 +1,29 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/15/2018
+
+/*Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+Example 1:
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+Input: [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum
+             jump length is 0, which makes it impossible to reach the last index.*/
+
+public class Solution {
+    public bool CanJump(int[] nums) {
+        int lastValidPos = nums.Length - 1;
+        for(int i = nums.Length - 2; i >= 0; i--)
+        {
+            if(i + nums[i] >= lastValidPos)
+            { lastValidPos = i; }
+        }
+        return lastValidPos == 0;
+    }
+}
