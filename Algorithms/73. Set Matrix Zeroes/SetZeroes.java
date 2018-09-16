b0d12f84b497fc81503dfaa894e6917bc086fe2d
Tushar <tjaiswal3@gatech.edu>
2018-09-16 15:24:33 -0500

    Used variables for array dimensions

--- Algorithms/73. Set Matrix Zeroes/SetZeroes.java
+++ Algorithms/73. Set Matrix Zeroes/SetZeroes.java
@@ -37,11 +37,13 @@ Could you devise a constant space solution?*/
 class Solution {
     public void setZeroes(int[][] matrix) {
         int col0 = 1;
-        for(int i = 0; i < matrix.length; i++)
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i = 0; i < m; i++)
         {
             if(matrix[i][0] == 0)
             { col0 = 0; }
-            for(int j = 1; j < matrix[0].length; j++)
+            for(int j = 1; j < n; j++)
             {
                 if(matrix[i][j] == 0)
                 {
@@ -50,21 +52,21 @@ class Solution {
                 }
             }
         }
-        for(int j = 1; j < matrix[0].length; j++)
+        for(int j = 1; j < n; j++)
         {
             if(matrix[0][j] == 0)
             {
-                for(int i = 0; i < matrix.length; i++)
+                for(int i = 0; i < m; i++)
                 {
                     matrix[i][j] = 0;
                 }
             }
         }
-        for(int i = 0; i < matrix.length; i++)
+        for(int i = 0; i < m; i++)
         {
             if(matrix[i][0] == 0)
             {
-                for(int j = 0; j < matrix[0].length; j++)
+                for(int j = 0; j < n; j++)
                 {
                     matrix[i][j] = 0;
                 }
