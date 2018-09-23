f5cdc8f0eea5f5675591688dd102bee1ee4ffaf5
Tushar <tjaiswal3@gatech.edu>
2018-09-23 12:28:37 -0500

    Created WordBreakBFS.java
    Java BFS solution for the Word Break problem.
    Problem Source: https://leetcode.com/problems/word-break

---
+++ Algorithms/139. Word Break/WordBreakBFS.java
@@ -0,0 +1,49 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/23/2018
+
+/*Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+Example 3:
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false*/
+
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> dict = new HashSet<String>(wordDict);
+        Queue<Integer> toVisit = new LinkedList<Integer>();
+        HashSet<Integer> visited = new HashSet<Integer>();
+        toVisit.offer(0);
+        while(!toVisit.isEmpty())
+        {
+            int pos = toVisit.poll();
+            if(!visited.contains(pos))
+            {
+                visited.add(pos);
+                for(int i = pos + 1; i <= s.length(); i++)
+                {
+                    if(dict.contains(s.substring(pos, i)))
+                    {
+                        if(i == s.length())
+                        { return true; }
+                        toVisit.offer(i);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
