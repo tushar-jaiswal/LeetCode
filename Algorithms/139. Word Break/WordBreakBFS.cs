2abbf9d57d727aedc39a583009c70ebd4e14d2e7
Tushar <tjaiswal3@gatech.edu>
2018-09-23 12:24:28 -0500

    Created WordBreakBFS.cs
    C# BFS solution for the Word Break problem.
    Problem Source: https://leetcode.com/problems/word-break

---
+++ Algorithms/139. Word Break/WordBreakBFS.cs
@@ -0,0 +1,49 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/23/2018
+
+/*Given a non-empty string s and a dictionary wordDict containing a list of non-empty words, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note:
+The same word in the dictionary may be reused multiple times in the segmentation.
+You may assume the dictionary does not contain duplicate words.
+
+Example 1:
+Input: s = "leetcode", wordDict = ["leet", "code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Example 2:
+Input: s = "applepenapple", wordDict = ["apple", "pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+             Note that you are allowed to reuse a dictionary word.
+Example 3:
+Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+Output: false*/
+
+public class Solution {
+    public bool WordBreak(string s, IList<string> wordDict) {
+        HashSet<string> dict = new HashSet<string>(wordDict);
+        Queue<int> toVisit = new Queue<int>();
+        HashSet<int> visited = new HashSet<int>();
+        toVisit.Enqueue(0);
+        while(toVisit.Count != 0)
+        {
+            int pos = toVisit.Dequeue();
+            if(!visited.Contains(pos))
+            {
+                visited.Add(pos);
+                for(int i = pos + 1; i <= s.Length; i++)
+                {
+                    if(dict.Contains(s.Substring(pos, i - pos)))
+                    {
+                        if(i == s.Length)
+                        { return true; }
+                        toVisit.Enqueue(i);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
