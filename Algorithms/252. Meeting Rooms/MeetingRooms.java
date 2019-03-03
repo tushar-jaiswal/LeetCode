dd2bae8d38ba053096bdc36654482c7124b828e0
Tushar <tjaiswal3@gatech.edu>
2019-03-03 14:46:36 -0600

    Removed Interval definition

--- Algorithms/252. Meeting Rooms/MeetingRooms.java
+++ Algorithms/252. Meeting Rooms/MeetingRooms.java
@@ -13,16 +13,6 @@ Output: true*/
 
 import java.util.*;
 
-/**
- * Definition for an interval.
- * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
- * }
- */
-
 public class MeetingRooms {
 
 	public static void main(String[] args) {
