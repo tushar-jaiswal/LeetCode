cc8f9c244b140d64510e397f0848a68dd2b1bcbc
Tushar <tjaiswal3@gatech.edu>
2019-04-07 11:33:52 -0500

    Simplified C# solution that removes unnecessary recursion.

--- Algorithms/33. Search in Rotated Sorted Array/Search.cs
+++ Algorithms/33. Search in Rotated Sorted Array/Search.cs
@@ -1,36 +1,44 @@
 //Author: Tushar Jaiswal
-//Creation Date: 03/29/2018
+//Creation Date: 04/07/2019
 
 /*Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
-(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+(i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
 You are given a target value to search. If found in the array return its index, otherwise return -1.
-You may assume no duplicate exists in the array.*/
+You may assume no duplicate exists in the array.
+Your algorithm's runtime complexity must be in the order of O(log n).
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1*/
 
 public class Solution {
     public int Search(int[] nums, int target) {
-        return Helper(nums, target, 0, nums.Length - 1);
-    }
-    
-    public int Helper(int[] nums, int target, int left, int right)
-    {
-        if(left > right)
-        { return -1; }
-        
-        int mid = left + (right - left) / 2;
-        if(nums[mid] == target)
-        { return mid; }
-        
-        if(nums[left] <= nums[mid])
+        int left = 0, right = nums.Length - 1;
+        while(left <= right)
         {
-            if(target >= nums[left] && target < nums[mid])
-            { return Helper(nums, target, left, mid - 1);}
-            return Helper(nums, target, mid + 1, right);
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+            { return mid; }
+
+            if(nums[left] <= nums[mid])
+            {
+                if(target >= nums[left] && target < nums[mid])
+                { right = mid - 1; }
+                else
+                { left = mid + 1; }
+            }
+            else
+            {
+                if(target > nums[mid] && target <= nums[right])
+                { left = mid + 1; }
+                else
+                { right = mid - 1; }
+            }
         }
-        else
-        {
-            if(target >= nums[mid + 1] && target <= nums[right])
-            { return Helper(nums, target, mid + 1, right); }
-            return Helper(nums, target, left, mid - 1);
-        }
+        return -1;
     }
 }
