9143e2d7176bf24fc757ea08d880ee989ffce557
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2020-09-03 23:33:19 -0700

    Created MergeTwoSortedListsOptimized.java
    Java optimized solution for the Merge Two Sorted Lists problem.
    Problem Source: https://leetcode.com/problems/merge-two-sorted-lists

---
+++ Algorithms/21. Merge Two Sorted Lists/MergeTwoSortedListsOptimized.java
@@ -0,0 +1,49 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/03/2020
+
+/*Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4*/
+
+/*Runtime Complexity: O(n) where n is total number of elements in both lists
+Space Complexity: O(1)*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode node = new ListNode(0);
+        ListNode merged = node;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                node.next = l1;
+                l1 = l1.next;
+            } else {
+                node.next = l2;
+                l2 = l2.next;
+            }
+            node = node.next;
+        }
+
+        if (l1 != null) {
+            node.next = l1;
+        }
+
+        if (l2 != null) {
+            node.next = l2;
+        }
+
+        return merged.next;
+    }
+}
