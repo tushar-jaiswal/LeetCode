9c25fe5d45bca7e51d444c75ffa8051253139be2
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2020-09-27 17:51:58 -0700

    Created IsMatch.cs
    C# solution for the Wildcard Matching problem.
    Problem Source: https://leetcode.com/problems/wildcard-matching

---
+++ Algorithms/44. Wildcard Matching/IsMatch.cs
@@ -0,0 +1,80 @@
+//Author: Tushar Jaiswal
+//Creation Date: 09/27/2020
+
+/*Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*'.
+
+'?' Matches any single character.
+'*' Matches any sequence of characters (including the empty sequence).
+The matching should cover the entire input string (not partial).
+
+Note:
+s could be empty and contains only lowercase letters a-z.
+p could be empty and contains only lowercase letters a-z, and characters like ? or *.
+
+Example 1:
+Input:
+s = "aa"
+p = "a"
+Output: false
+Explanation: "a" does not match the entire string "aa".
+
+Example 2:
+Input:
+s = "aa"
+p = "*"
+Output: true
+Explanation: '*' matches any sequence.
+
+Example 3:
+Input:
+s = "cb"
+p = "?a"
+Output: false
+Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+Example 4:
+Input:
+s = "adceb"
+p = "*a*b"
+Output: true
+Explanation: The first '*' matches the empty sequence, while the second '*' matches the substring "dce".
+
+Example 5:
+Input:
+s = "acdcb"
+p = "a*c?b"
+Output: false*/
+
+/*Runtime Complexity: O(length of string * length of pattern)
+Space Complexity: O(1)*/
+
+public class Solution {
+    public bool IsMatch(string s, string p) {
+        int i = 0;
+        int j = 0;
+        int starIndex = -1;
+        int stringMatchStartIndex = -1;
+        while (i < s.Length) {
+            if (j < p.Length && (s[i] == p[j] || p[j] == '?')) {
+                i++;
+                j++;
+            } else if (j < p.Length && p[j] == '*') {
+                starIndex = j;
+                stringMatchStartIndex = i;
+                j++;
+            } else if (starIndex != -1) {
+                j = starIndex + 1;
+                stringMatchStartIndex++;
+                i = stringMatchStartIndex;
+            } else {
+                return false;
+            }
+        }
+
+        while (j < p.Length && p[j] == '*') {
+            j++;
+        }
+
+        return (j == p.Length);
+    }
+}
