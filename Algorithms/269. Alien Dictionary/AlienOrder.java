3687d8518cfa7e16fce05b15af0d48a58185e80b
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2020-11-29 15:04:00 -0800

    Updated Java solution for Alien Dictionary problem
    Updated Java solution for the Alien Dictionary problem which handles an
    additional edge case where a word is a prefix of a word before it in
    the input word array.

--- Algorithms/269. Alien Dictionary/AlienOrder.java
+++ Algorithms/269. Alien Dictionary/AlienOrder.java
@@ -1,114 +1,114 @@
 //Author: Tushar Jaiswal
-//Creation Date: 06/23/2019
+//Creation Date: 11/28/2020
 
-/*There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+/*There is a new alien language that uses the English alphabet. However, the order among letters are unknown to you.
+
+You are given a list of strings words from the dictionary, where words are sorted lexicographically by the rules of this new language.
+
+Derive the order of letters in this language, and return it. If the given input is invalid, return "". If there are multiple valid solutions, return any of them.
+
 Example 1:
-Given the following words in dictionary,
-[
-  "wrt",
-  "wrf",
-  "er",
-  "ett",
-  "rftt"
-]
-The correct order is: "wertf".
+Input: words = ["wrt","wrf","er","ett","rftt"]
+Output: "wertf"
 
 Example 2:
-Given the following words in dictionary,
-[
-  "z",
-  "x"
-]
-The correct order is: "zx".
+Input: words = ["z","x"]
+Output: "zx"
 
 Example 3:
-Given the following words in dictionary,
-[
-  "z",
-  "x",
-  "z"
-]
-The order is invalid, so return "".
+Input: words = ["z","x","z"]
+Output: ""
+Explanation: The order is invalid, so return "".
 
-Note:
-You may assume all letters are in lowercase.
-You may assume that if a is a prefix of b, then a must appear before b in the given dictionary.
-If the order is invalid, return an empty String.
-There may be multiple valid order of letters, return any one of them is fine.*/
+Constraints:
+    1 <= words.length <= 100
+    1 <= words[i].length <= 100
+    words[i] consists of only lowercase English letters.*/
 
-import java.util.*;
+/*Runtime Complexity: O(sum of lengths of all words)
+Space Complexity: O(U + min(U^2,N)) where U is the total number of unique letters in the alien alphabet and N is the total number of strings in the input list.*/
 
-public class AlienDictionary {
+class Solution {
+    public String alienOrder(String[] words) {
+        if (words.length == 1) {
+            return words[0];
+        }
 
-	public static void main(String[] args) {
-		System.out.println(alienOrder(new String[] { "wrt", "wrf", "er", "ett", "rftt" }));
-		System.out.println(alienOrder(new String[] { "z", "x" }));
-		System.out.println(alienOrder(new String[] { "z", "x" , "z"}));
-	}
+        var orderMap = new HashMap<Character, HashSet<Character>>();
+        HashMap<Character, Integer> inDegreeMap = createInDegreeMap(words);
 
-	public static String alienOrder(String[] words)
-    {
-        HashMap<Character, List<Character>> graph = new HashMap<Character, List<Character>>();
-        HashMap<Character, Integer> inDegree = new HashMap<Character, Integer>();
-        createGraph(words, graph, inDegree);
-        String order = topologicalSorting(graph, inDegree);
-        return order.length() == graph.size() ? order : "";
-    }
-
-    private static void createGraph(String[] words, HashMap<Character, List<Character>> graph, HashMap<Character, Integer> inDegree)
-    {
-        for (String word : words)
-        {
-            for (char c : word.toCharArray())
-            {
-                if (!graph.containsKey(c))
-                { graph.put(c, new ArrayList<Character>()); }
-                if (!inDegree.containsKey(c))
-                { inDegree.put(c, 0); }
+        for (int i = 0; i < words.length - 1; i++) {
+            if (!compareWords(words[i], words[i + 1], orderMap, inDegreeMap)) {
+                return "";
             }
         }
 
-        for (int i = 1; i < words.length; i++)
-        {
-            String first = words[i - 1];
-            String second = words[i];
-            int minLength = Math.min(first.length(), second.length());
+        String result = topologicalSorting(orderMap, inDegreeMap);
+        int visitedCharsCount = result.length();
+        int totalCharsCount = inDegreeMap.size();
 
-            for (int j = 0; j < minLength; j++)
-            {
-                char parent = first.charAt(j);
-                char child = second.charAt(j);
-                if (parent != child)
-                {
-                    graph.get(parent).add(child);
-                    inDegree.put(child, inDegree.get(child) + 1);
+        return visitedCharsCount == totalCharsCount ? result : "";
+    }
+
+    private HashMap<Character, Integer> createInDegreeMap(String[] words) {
+        HashMap<Character, Integer> inDegreeMap = new HashMap<Character, Integer>();
+        for (String word: words) {
+            for (char c : word.toCharArray())
+            inDegreeMap.put(c, 0);
+        }
+
+        return inDegreeMap;
+    }
+
+    private boolean compareWords(String word1, String word2, HashMap<Character, HashSet<Character>> orderMap, HashMap<Character, Integer> inDegreeMap) {
+        int len1 = word1.length();
+        int len2 = word2.length();
+        boolean noCharDiff = true;
+
+        int i = 0;
+        for (; i < Math.min(len1, len2); i++) {
+            char c1 = word1.charAt(i);
+            char c2 = word2.charAt(i);
+            if (c1 != c2) {
+                noCharDiff = false;
+                if (!orderMap.containsKey(c1)) {
+                    orderMap.put(c1, new HashSet<Character>());
+                }
+                if (!orderMap.get(c1).contains(c2)) {
+                    orderMap.get(c1).add(c2);
+                    inDegreeMap.merge(c2, 1, Integer::sum);
+                }
+                break;
+            }
+        }
+
+        return len1 > len2 && noCharDiff ? false : true;
+    }
+
+    private String topologicalSorting(HashMap<Character, HashSet<Character>> orderMap, HashMap<Character, Integer> inDegreeMap) {
+        var queue = new ArrayDeque<Character>();
+
+        for (var entry : inDegreeMap.entrySet()) {
+            if (entry.getValue() == 0) {
+                queue.add(entry.getKey());
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char c = queue.remove();
+            result.append(c);
+
+            if (orderMap.containsKey(c)) {
+                for (char child : orderMap.get(c)) {
+                    inDegreeMap.merge(child, -1, Integer::sum);
+                    if (inDegreeMap.get(child) == 0) {
+                        queue.add(child);
+                    }
                 }
             }
         }
-    }
 
-    private static String topologicalSorting(HashMap<Character, List<Character>> graph, HashMap<Character, Integer> inDegree)
-    {
-        Queue<Character> queue = new LinkedList<Character>();
-        String order = "";
-
-        for (Map.Entry<Character, Integer> entry : inDegree.entrySet())
-        {
-            if(entry.getValue() == 0)
-            { queue.offer(entry.getKey()); }
-        }
-        while(!queue.isEmpty())
-        {
-            char c = queue.poll();
-            order += c;
-            for(char child :graph.get(c))
-            {
-            	int degree = inDegree.get(child) - 1;
-                inDegree.put(child, degree);
-                if(degree == 0)
-                { queue.offer(child); }
-            }
-        }
-        return order;
+        return result.toString();
     }
 }
