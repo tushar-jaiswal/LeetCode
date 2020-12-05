9fef654dbdf42575aa16893b1f54462cb77ec515
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2020-12-05 15:42:07 -0800

    Java Binary Search solution for Leftmost Column with at Least a One
    Java Binary Search solution for the Leftmost Column with at Least a One
    problem.
    Problem Source: https://leetcode.com/problems/leftmost-column-with-at-least-a-one

---
+++ Algorithms/1428. Leftmost Column with at Least a One/LeftMostColumnWithOneBinarySearch.java
@@ -0,0 +1,80 @@
+//Author: Tushar Jaiswal
+//Creation Date: 12/05/2020
+
+/*(This problem is an interactive problem.)
+
+A binary matrix means that all elements are 0 or 1. For each individual row of the matrix, this row is sorted in non-decreasing order.
+
+Given a row-sorted binary matrix binaryMatrix, return leftmost column index(0-indexed) with at least a 1 in it. If such index doesn't exist, return -1.
+
+You can't access the Binary Matrix directly.  You may only access the matrix using a BinaryMatrix interface:
+    BinaryMatrix.get(row, col) returns the element of the matrix at index (row, col) (0-indexed).
+    BinaryMatrix.dimensions() returns a list of 2 elements [rows, cols], which means the matrix is rows * cols.
+
+Submissions making more than 1000 calls to BinaryMatrix.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+For custom testing purposes you're given the binary matrix mat as input in the following four examples. You will not have access the binary matrix directly.
+
+Example 1:
+Input: mat = [[0,0],[1,1]]
+Output: 0
+
+Example 2:
+Input: mat = [[0,0],[0,1]]
+Output: 1
+
+Example 3:
+Input: mat = [[0,0],[0,0]]
+Output: -1
+
+Example 4:
+Input: mat = [[0,0,0,1],[0,0,1,1],[0,1,1,1]]
+Output: 1
+
+Constraints:
+    rows == mat.length
+    cols == mat[i].length
+    1 <= rows, cols <= 100
+    mat[i][j] is either 0 or 1.
+    mat[i] is sorted in a non-decreasing way.
+*/
+
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int row, int col) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
+
+/*Runtime Complexity: O((num of rows) * log(num of columns)) on average for all operations.
+Space Complexity: O(1)*/
+
+class Solution {
+    public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
+        List<Integer> dimensions = binaryMatrix.dimensions();
+        int numRows = dimensions.get(0);
+        int numCols = dimensions.get(1);
+
+        int earliestCol = numCols;
+
+        for (int i = 0; i < numRows; i++) {
+            int low = 0, high = earliestCol - 1;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                int item = binaryMatrix.get(i, mid);
+                if (item == 0) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                    earliestCol = Math.min(earliestCol, mid);
+                }
+            }
+        }
+
+        return earliestCol == numCols ? -1 : earliestCol;
+    }
+}
