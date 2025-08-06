fe2ab343fc4987e26c52591bab68855c81e933b9
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2025-08-05 18:30:13 -0700

    Create SearchRangeLibraryBS.py
    Library Binary Search solution for the Find First and Last Position of Element in Sorted Array problem.
    
    Problem Source: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array

---
+++ Algorithms/34. Search for a Range/SearchRangeLibraryBS.py
@@ -0,0 +1,37 @@
+# Author: Tushar Jaiswal
+# Creation Date: 2025-08-05
+
+# Problem Source: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array
+
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+
+# Example 2:
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+
+# Example 3:
+# Input: nums = [], target = 0
+# Output: [-1,-1]
+
+# Constraints:
+# 0 <= nums.length <= 10^5
+# -10^9 <= nums[i] <= 10^9
+# nums is a non-decreasing array.
+# -10^9 <= target <= 10^9
+
+# Runtime complexity: O(log n)
+# Space complexity: O(1) 
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = bisect.bisect_left(nums, target)
+        right = bisect.bisect_right(nums, target)
+        return [left, right - 1] if left != right else [-1, -1]
