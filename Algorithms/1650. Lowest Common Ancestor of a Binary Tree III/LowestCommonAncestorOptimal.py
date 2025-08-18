34c86fb0902d6b37d5329e8fbacc4c7f3e0bc188
Tushar Jaiswal <tushar-jaiswal@users.noreply.github.com>
2025-08-17 21:29:12 -0700

    Added reasoning for why solution works and its intuition

--- Algorithms/1650. Lowest Common Ancestor of a Binary Tree III/LowestCommonAncestorOptimal.py
+++ Algorithms/1650. Lowest Common Ancestor of a Binary Tree III/LowestCommonAncestorOptimal.py
@@ -57,3 +57,24 @@ class Solution:
             p2 = p2.parent if p2.parent else p
 
         return p1
+
+# Reason this works
+# For anyone struggling to see it, imagine to nodes, p and q, whose paths merge and become common after a certain number of steps.
+# p1 -> p2 -> p3 -> c1 -> c2 -> c3
+# q1 -> q2 -> q3 -> c1 -> c2 -> c3
+# If the distance from p1 to c1 is the same as the distance from q1 to c1, it's pretty obvious this algorithm will find when c1 == c1.
+
+# But now imagine those distances are different.
+# p1 -> p2 -> p3 -> c1 -> c2 -> c3
+# .........................q1 -> c1 -> c2 -> c3
+
+# If you force them to switch paths after they reach c3:
+# P Travels: (3 steps to c1), (3 common steps to q1), (1 step to c1)
+# Q Travels: (1 step to c1), (3 common steps to p1), (3 steps to c1)
+
+# OR put another way
+
+# P Travels: PC, C, QC
+# Q Travels: QC, C, PC
+
+# where C is the common paths. PC is p's unique path to the common ancestor. QC is q's unique path.
